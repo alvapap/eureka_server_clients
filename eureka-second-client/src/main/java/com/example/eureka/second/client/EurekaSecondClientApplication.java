4652cd9c4f7f14e2a93fa16e0d02280cf2d21b5f
alvapap <alvapap@hotmail.com>
2018-05-03 10:23:18 +0200

    adding hystrix and hystrix dashboard

--- eureka-second-client/src/main/java/com/example/eureka/second/client/EurekaSecondClientApplication.java
+++ eureka-second-client/src/main/java/com/example/eureka/second/client/EurekaSecondClientApplication.java
@@ -2,13 +2,15 @@ package com.example.eureka.second.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableCircuitBreaker
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class EurekaSecondClientApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EurekaSecondClientApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaSecondClientApplication.class, args);
+	}
 }
--- eureka-second-client/src/main/java/com/example/eureka/second/client/controller/EurekaAddressClientController.java
+++ eureka-second-client/src/main/java/com/example/eureka/second/client/controller/EurekaAddressClientController.java
@@ -1,14 +1,11 @@
 package com.example.eureka.second.client.controller;
 
-import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
-import com.netflix.discovery.shared.Application;
+import com.netflix.discovery.shared.Applications;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -20,14 +17,10 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
 public class EurekaAddressClientController {
-
-    @Autowired
-    private DiscoveryClient discoveryClient;
 
     @Autowired
     private EurekaClient eurekaClient;
@@ -38,11 +31,12 @@ public class EurekaAddressClientController {
     private final String EUREKA_NAME_CLIENT = "EUREKA-NAME-CLIENT";
 
     @RequestMapping("/service-instances/{applicationName}")
-    public List<ServiceInstance> serviceInstancesByApplicationName(
+    public Applications serviceInstancesByApplicationName(
             @PathVariable String applicationName) {
-        return this.discoveryClient.getInstances(applicationName);
+        return this.eurekaClient.getApplications(applicationName);
     }
 
+    @HystrixCommand(fallbackMethod = "getNamesBackup")
     @RequestMapping("/addresses")
     public List<String> getAddresses() {
 
@@ -58,10 +52,20 @@ public class EurekaAddressClientController {
 
         List<String> combined = nameResponse.getBody()
                 .stream()
-                .map(name -> name + " lives in " + addresses.get(nameResponse	.getBody().indexOf(name)))
+                .map(name -> name + " lives in " + addresses.get(nameResponse.getBody().indexOf(name)))
                 .collect(Collectors.toList());
 
         return combined;
+    }
+
+    public List<String> getNamesBackup() {
+        System.out.println("Fallback operation");
+
+        List<String> nameAddresses = new ArrayList<>();
+
+        nameAddresses.add("Nobody lives here at the moment");
+
+        return nameAddresses;
     }
 
     @LoadBalanced
